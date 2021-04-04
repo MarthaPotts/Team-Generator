16cc16925bf849954396a7cbbdbfe66c1a467b91
Martha Potts <marthajpotts7@gmail.com>
2021-04-04 00:59:08 -0400

    added style, updated file structure

--- index.js
+++ index.js
@@ -5,10 +5,8 @@ const Engineer = require('./Lib/engineer');
 const Intern = require('./Lib/intern');
 const Manager = require('./Lib/manager');
 const fs = require('fs');
-//prompt! to enter Team Manager info//
 
-//Menu: option: addEngineer()? addIntern()? finishTeam()?
-
+//Gather input from User about Engineer
 async function addEngineer(employees){
    const engineer = await inquirer.prompt([
         {
@@ -32,10 +30,11 @@ async function addEngineer(employees){
         message: 'What is the Github Username of the Engineer?',
     }
 ]);
+//push input into employees array which holds all of employee related user input
  const eng = new Engineer(engineer.engrName, engineer.engrId, engineer.engrEmail, engineer.engrGithub); 
  employees.push(eng);
 }
-
+//gather user input about interns: 
 async function addIntern(employees){
     const intern = await inquirer.prompt([
         {
@@ -59,9 +58,11 @@ async function addIntern(employees){
             message: 'What is the name of the school your Intern is attending?', 
         }
     ]);
+    //push to employees array
      const student = new Intern(intern.internName, intern.internId, intern.internEmail, intern.internSchool);
      employees.push(student);
 }
+//write html file populated with user input from employess array: 
 function generateHTML(employees) {
     const employeeCards =[];
     for(let i = 0; i < employees.length; i++){
@@ -69,25 +70,25 @@ function generateHTML(employees) {
             employeeCards.push(`<div class="card">
             <h2>${employees[i].getName()}</h2>
             <h3>${employees[i].getRole()}</h3>
-            <h3>${employees[i].getId()}</h3>
-            <h3>${employees[i].getEmail()}</h3>
-            <h3>${employees[i].officeNumber}</h3>
+            <h3>Employee Id: ${employees[i].getId()}</h3>
+            <h3>Email: ${employees[i].getEmail()}</h3>
+            <h3>Office Phone: ${employees[i].officeNumber}</h3>
             </div>`)
         } else if (employees[i].getRole() === "Engineer"){
             employeeCards.push(`<div class="card">
             <h2>${employees[i].getName()}</h2>
             <h3>${employees[i].getRole()}</h3>
-            <h3>${employees[i].getId()}</h3>
-            <h3>${employees[i].getEmail()}</h3>
-            <h3>${employees[i].getGithub()}</h3>
+            <h3>Employee Id: ${employees[i].getId()}</h3>
+            <h3>Email: ${employees[i].getEmail()}</h3>
+            <h3>Github Profile: ${employees[i].getGithub()}</h3>
             </div>`)
         } else {
             employeeCards.push(`<div class="card">
             <h2>${employees[i].getName()}</h2>
             <h3>${employees[i].getRole()}</h3>
-            <h3>${employees[i].getId()}</h3>
-            <h3>${employees[i].getEmail()}</h3>
-            <h3>${employees[i].getSchool()}</h3>
+            <h3>Employee Id: ${employees[i].getId()}</h3>
+            <h3>Email: ${employees[i].getEmail()}</h3>
+            <h3>School: ${employees[i].getSchool()}</h3>
             </div>`)
         }
     }
@@ -98,6 +99,30 @@ function generateHTML(employees) {
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Team Builder</title>
+        <style>
+            header {
+                width: auto; 
+                height: 200px;
+                background-color: burlywood;
+            }
+            h1 {
+                text-align: center;
+                padding-top: 80px;
+            }
+            #container {
+                display: flex;
+                flex-flow: row wrap;
+                justify-content: center;
+                align-items: baseline;
+                background-color: cadetblue;
+            }
+            .card {
+                border: thin solid black;
+                margin: 5px;
+                display: flex;
+                flex-flow: column nowrap; 
+            }
+        </style>
     </head>
     <body>
         <header>
@@ -105,7 +130,7 @@ function generateHTML(employees) {
                 Software Engineering Team
             </h1>
         </header>
-        <section>`;
+        <section id="container">`;
             employeeCards.forEach(card => {
                 html += card;
             });
