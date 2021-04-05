42678949ca2dff6613511f6322f19a76a4b976f3
Martha Potts <marthajpotts7@gmail.com>
2021-04-05 11:13:07 -0400

    uploading to save before potential destruction

--- __tests__/employee.test.js
+++ __tests__/employee.test.js
@@ -1,6 +1,6 @@
 import Employee from '/Lib/employee.js';
 //method added to class to retrieve name, should return name from input data returned via inquirer
-descibe("function getName", () => {
+describe("function getName", () => {
     test("should return 'name' from result", () => {
 
     })
@@ -11,3 +11,6 @@ descibe("function getName", () => {
 //method getEmail, should return Email from data
 
 //method get role, should return specified role depending on subclass
+
+//typical test flow should: import the function to test, give an input to the function, define what to expect as output, check if function produces expected output
+//arrange/act/assert... input - expected output - assert the result
